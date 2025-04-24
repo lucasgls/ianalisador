b72cc841979c38bcd34a2be8cbc651be82208f99
Lucas Gabriel <lucasglsilva7@gmail.com>
2025-04-24 12:27:14 -0300

    Integrate Azure AI Services, Add Dependencies, and Configure .env for Secure Credentials

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
+import pandas as pd
 import streamlit as st
 import numpy as np
 from PIL import Image
 import requests
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from a .env file
+load_dotenv()
 
 nomeServico = "IAnalisador 👀"
 
 with st.sidebar:
     st.title("Vaga a ser preenchida 👇")
-    chart_selection = st.selectbox("Selecione a vaga que deseja Preencher",
-                                   ("Vendedor", "Gerente de Vendas"))
-    
+    chart_selection = st.selectbox("Selecione a vaga que deseja Preencher", 
+                                    ("Vendedor", "Gerente de Vendas")
+                                    )
 
 st.title(f"Seja bem vindo ao {nomeServico}");
 st.write("Analisarei os dados do candidato e irei mandar dados relevantes sobre ele a seguir! ") 
